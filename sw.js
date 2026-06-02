15d173d475a25d0f7703a664ce82c2f2cf8230c5
phantanont12-sudo <phantanont12@gmail.com>
2026-06-02 12:13:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'care-nst-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
